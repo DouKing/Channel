6c6fb2c007a73913768e10d7b994e36dabf4fe6c
douking <wyk8916@gmail.com>
2025-02-19 20:48:41 +0800

    alert extension

--- Source/Extension/UIAlertControllerExtension.swift
+++ Source/Extension/UIAlertControllerExtension.swift
@@ -10,9 +10,31 @@
 import UIKit
 
 extension UIAlertAction {
+    private enum Key: String {
+        case titleTextColor
+        case titleTextAlignment
+        case image
+        case imageTintColor
+    }
+    
     public var titleTextColor: UIColor? {
-        get { value(forKey: "titleTextColor") as? UIColor }
-        set { setValue(newValue, forKey: "titleTextColor") }
+        get { value(forKey: Key.titleTextColor.rawValue) as? UIColor }
+        set { setValue(newValue, forKey: Key.titleTextColor.rawValue) }
+    }
+    
+    public var titleTextAlignment: NSTextAlignment? {
+        get { value(forKey: Key.titleTextAlignment.rawValue) as? NSTextAlignment }
+        set { setValue(newValue, forKey: Key.titleTextAlignment.rawValue) }
+    }
+    
+    public var image: UIImage? {
+        get { value(forKey: Key.image.rawValue) as? UIImage }
+        set { setValue(newValue, forKey: Key.image.rawValue) }
+    }
+    
+    public var imageTintColor: UIColor? {
+        get { value(forKey: Key.imageTintColor.rawValue) as? UIColor }
+        set { setValue(newValue, forKey: Key.imageTintColor.rawValue) }
     }
 }
 
@@ -20,6 +42,29 @@ extension UIAlertAction {
     public func withTitleTextColor(_ color: UIColor?) -> UIAlertAction {
         titleTextColor = color
         return self
+    }
+}
+
+extension UIAlertController {
+    private enum Key: String {
+        case attributedTitle
+        case attributedMessage
+        case contentViewController
+    }
+    
+    public var attributedTitle: NSAttributedString? {
+        get { value(forKey: Key.attributedTitle.rawValue) as? NSAttributedString }
+        set { setValue(newValue, forKey: Key.attributedTitle.rawValue) }
+    }
+    
+    public var attributedMessage: NSAttributedString? {
+        get { value(forKey: Key.attributedMessage.rawValue) as? NSAttributedString }
+        set { setValue(newValue, forKey: Key.attributedMessage.rawValue) }
+    }
+    
+    public var contentViewController: UIViewController? {
+        get { value(forKey: Key.contentViewController.rawValue) as? UIViewController }
+        set { setValue(newValue, forKey: Key.contentViewController.rawValue) }
     }
 }
 
